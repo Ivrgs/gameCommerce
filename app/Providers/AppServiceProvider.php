0a5446aa57fa3fd7616a6fedf67be0a4e65f69eb
Ivan Vargas <31211873+Ivrgs@users.noreply.github.com>
2026-07-01 10:08:52 +0800

    Vercel Setup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,8 +22,15 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
-        Schema::defaultStringLength(191);
+    // public function boot()
+    // {
+    //     Schema::defaultStringLength(191);
+        
+    // }
+    public function boot(): void
+{
+    if (config('app.env') === 'production') {
+        $this->app->useStoragePath('/tmp/storage');
     }
+}
 }
